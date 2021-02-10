ac8f9dfba890932a3a239ed9de1ccd43c34776ec
sformichella <sformichella@gmail.com>
2021-02-09 16:18:16 -0800

    add getCharacters to useCharacters

--- src/hooks/characters.js
+++ src/hooks/characters.js
@@ -1,12 +1,16 @@
 import { useState, useEffect } from 'react';
+import { getCharacters, getCharacter } from '../services/rickAndMorty';
 
 const useCharacters = () => {
   const [characters, setCharacters] = useState([]);
-  const [loading, setLoading] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-
-  });
+    getCharacters().then(chars => {
+      setCharacters(chars);
+      setLoading(false);
+    });
+  }, []);
 
   return { characters, loading };
 };

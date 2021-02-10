7d09743122d841d467c1c4344636291c79f92051
sformichella <sformichella@gmail.com>
2021-02-09 16:24:12 -0800

    useCharacter in DetailPage

--- src/components/pages/DetailPage/DetailPage.jsx
+++ src/components/pages/DetailPage/DetailPage.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../../presentation/Header/Header';
 import Details from '../../presentation/Details/Details';
+import { useCharacter } from '../../../hooks/characters';
 
-export default function DetailPage() {
+export default function DetailPage({ match }) {
+
+  const { id } = match.params;
+  const { character, loading } = useCharacter(id);
+
   return (
     <>
       <Header />
-      <Details {...character}/>
+      {
+        loading ? <p>Loading!</p>
+          : <Details {...character}/>
+      }
     </>
   );
 }
+
+DetailPage.propTypes = {
+  match: PropTypes.objectOf(PropTypes.shape({
+    params: PropTypes.objectOf(PropTypes.shape({
+      id: PropTypes.string.isRequired
+    }))
+  }))
+};
